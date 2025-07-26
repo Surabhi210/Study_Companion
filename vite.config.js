37513cc9a80ac257a12c905afaec71d7656bf861
Surabhi Sethi <surabhisethi210@gmail.com>
2025-07-26 19:04:03 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Study_Companion/',
-  plugins: [react()]
+  plugins: [react()],
 })
